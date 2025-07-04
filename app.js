ab703ce3e0124a1eb37058f620977954c4be512e
Paul levites <Paullevites84@gmail.com>
2025-07-04 19:24:28 +0100

    fixes

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const carousel = document.getElementById("images");
 const nextBtn = document.getElementById("nextBtn");
 const prevBtn = document.getElementById("prevBtn");
-// const toggleBtn = document.getElementById("menu-toggle");
-// const nav = document.querySelector("nav");
-// const navLinks = document.querySelectorAll(".nav-link");
 
 const scrollAmount = 320;
 
@@ -14,17 +11,3 @@ nextBtn.addEventListener("click", () => {
 prevBtn.addEventListener("click", () => {
   carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });
 });
-
-// NavBar
-// toggleBtn.addEventListener("click", () => {
-//   nav.classList.toggle("open");
-// });
-
-// navLinks.forEach((link) => {
-//   link.addEventListener("click", () => {
-//     nav.classList.remove("open");
-
-//     navLinks.forEach((l) => l.classList.remove("active"));
-//     link.classList.add("active");
-//   });
-// });
